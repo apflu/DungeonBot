8b44172641515c5d7e821f8be1c70e6542e27888
apflu <apflufadalia@gmail.com>
2019-10-04 00:44:24 +0200

    change dice to non-static

--- Native.Csharp/App/Util/Math/Dice.cs
+++ Native.Csharp/App/Util/Math/Dice.cs
@@ -6,19 +6,23 @@ using System.Threading.Tasks;
 
 namespace Native.Csharp.App.Util.Math
 {
-    public static class Dice
+    public class Dice
     {
-        public static int GetResult(int intSize)
+        private readonly int Quantity;
+        private readonly int Size;
+
+        private const int Quantity_Max = 100;
+        private const int Size_Max = 100;
+
+        public Dice(int intQuantity, int intSize)
         {
-            Random random = new Random();
-            return random.Next(1, intSize); //from 1 to size
+            Quantity = (intQuantity > Quantity_Max) ? Quantity_Max : intQuantity;
+            Size = (intSize > Size_Max) ? Size_Max : intSize;
         }
 
-        public static int GetDice(String strDice)
+        public Dice(string strDice)
         {
-            int Result = 0;
-
-            String[] strDiceSet = new String[2]; //0 Quantity 1 Size
+            string[] strDiceSet = new string[2]; //0 Quantity 1 Size
             if (strDice[0].Equals('d'))
             {
                 strDiceSet[0] = "1";
@@ -26,21 +30,51 @@ namespace Native.Csharp.App.Util.Math
             }
             else strDiceSet = strDice.Split('d');
 
-            //casting String to Integer
-            int[] intDiceSet = new int[2]; //0 Quantity 1 Size
-            intDiceSet[0] = int.Parse(strDiceSet[0]);
-            intDiceSet[1] = int.Parse(strDiceSet[1]);
+            Quantity = int.Parse(strDiceSet[0]);
+            Size = int.Parse(strDiceSet[1]);
+        }
 
-            //limit maximum dice to d100
-            if (intDiceSet[1] > 100) intDiceSet[1] = 100;
+        private int GetDice(int intSize)
+        {
+            Random random = new Random();
+            return random.Next(1, intSize); //from 1 to size
+        }
+
+        /// <summary>
+        /// 0 = final result
+        /// </summary>
+        /// <returns></returns>
+        public int[] GetResult()
+        {
+            int[] Result = new int[Quantity + 1];
+
+            int dice;
 
             //randomize and get result
-            for (int i = 0; i < intDiceSet[0]; i++)
+            for (int i = 1; i < Size; i++)
             {
-                Result += GetResult(intDiceSet[1]);
+                dice = GetDice(Size);
+
+                Result[0] += dice;
+                Result[i] = dice;
             }
 
             return Result;
+        }
+
+        public int GetQuantity()
+        {
+            return Quantity;
+        }
+
+        public int GetSize()
+        {
+            return Size;
+        }
+
+        public override string ToString()
+        {
+            return Quantity + "d" + Size;
         }
     }
 }
