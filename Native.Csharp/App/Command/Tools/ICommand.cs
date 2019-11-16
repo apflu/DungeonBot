8e25087d51b1e13e4d6ecdc26edf8ae12f8a7bea
uziym <uziym@student.kit.edu>
2019-11-16 22:54:27 +0100

    添加命令接口

---
+++ Native.Csharp/App/Command/Tools/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using Native.Csharp.App.Gameplay;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Native.Csharp.App.Command
+{
+    public interface ICommand
+    {
+        void Execute(Player sender, params string[] args);
+    }
+}
