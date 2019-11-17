fe119515983c7f259997162c65682988db80ef9c
apflu <apflufadalia@gmail.com>
2019-11-17 15:14:00 +0100

    添加HP选项

--- Native.Csharp/App/Gameplay/CharacterProperties.cs
+++ Native.Csharp/App/Gameplay/CharacterProperties.cs
@@ -10,6 +10,9 @@ namespace Native.Csharp.App.Gameplay
     {
         public byte STR, DEX, CON, INT, WIS, CHA;
 
+        public byte HPCurrent;
+        public byte HPMax;
+
         /// <summary>
         /// 某些情况下（例如不死生物和构造体）不存在某项属性，
         /// 那么就将其设置为255
@@ -28,6 +31,32 @@ namespace Native.Csharp.App.Gameplay
             this.INT = INT;
             this.WIS = WIS;
             this.CHA = CHA;
+        }
+
+        public CharacterProperties(byte STR, byte DEX, byte CON, byte INT, byte WIS, byte CHA, byte maxHP)
+            :this(STR, DEX, CON, INT, WIS, CHA)
+        {
+            HPMax = maxHP;
+        }
+
+        /// <summary>
+        /// 检查角色是否已经能够被使用（以及能够行动）
+        /// 伊利丹梗
+        /// </summary>
+        /// <returns>是否自寻死路</returns>
+        public bool IsPrepared()
+        {
+            return (
+                (STR > 0) &&
+                (DEX > 0) &&
+                (CON > 0) &&
+                (INT > 0) &&
+                (WIS > 0) &&
+                (CHA > 0) &&
+                
+                (HPMax > 0) &&
+                (HPCurrent > 0)
+                );
         }
     }
 }
