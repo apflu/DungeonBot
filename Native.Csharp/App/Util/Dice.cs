fb5850fc0dbe47ecfe5df37b28c75ec7356d4c5e
apflu <apflufadalia@gmail.com>
2019-11-17 01:18:34 +0100

    添加骰子类

---
+++ Native.Csharp/App/Util/Dice.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Native.Csharp.App.Util
+{
+    public class Dice
+    {
+        public byte Quantity { get; set; }
+        public byte Size { get; protected set; }
+
+        private readonly Random random;
+        public Dice(byte quantity, byte size)
+        {
+            Quantity = quantity;
+            Size = size;
+
+            random = new Random();
+        }
+
+        public DiceResult GetResult()
+        {
+            string strResult = Quantity + "d" + Size + "=";
+            int intResult = 0;
+            int result;
+            for(int i = 0; i < Quantity; i++)
+            {
+                result = random.Next(Size) + 1;
+                strResult += result + "+";
+                intResult += result;
+            }
+            return new DiceResult(strResult.TrimEnd('+'), intResult);
+        }
+    }
+}
---
+++ Native.Csharp/App/Util/DiceResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Native.Csharp.App.Util
+{
+    public class DiceResult
+    {
+        public string StrResult { get; private set; }
+        public int IntResult { get; private set; }
+        public DiceResult(string strResult, int intResult)
+        {
+            StrResult = strResult + "=" + intResult;
+            IntResult = intResult;
+        }
+    }
+}
